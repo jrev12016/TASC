ceaeb115da0e2a6f8cc9f5dd532a233201044e08
Jacob Revis <122620894+jrev12016@users.noreply.github.com>
2023-11-07 19:32:44 +0000

    added password back to user

--- TASC_Code/main.py
+++ TASC_Code/main.py
@@ -19,7 +19,8 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-
+    password = db.Column(db.String(50), nullable=False)
+    
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
